da66646b9597155cead0095d3a83ea16b289a7c3
朱杰 <optimodzhu@outlook.com>
2020-04-29 19:22:41 +0800

    temp

--- Capitalino/Capitalino.Traiano/App_Start/CmdConfigs.cs
+++ Capitalino/Capitalino.Traiano/App_Start/CmdConfigs.cs
@@ -1,4 +1,12 @@
-﻿using Autodesk.AutoCAD.Runtime;
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Capitalino.Traiano.Block;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+
 [assembly: CommandClass(typeof(Capitalino.Traiano.CmdConfigs))]
 namespace Capitalino.Traiano
 {
@@ -8,5 +16,42 @@ namespace Capitalino.Traiano
         public static void ZPRINTSINGLE() => PublicationCmds.PrintSingle();
         [CommandMethod("ZPRINTMULTI")]
         public static void ZPRINTMULTI() => PublicationCmds.PrintMulti();
+        [CommandMethod("ZCOUNTBLOCK")]
+        public static void ZCOUNTBLOCK() => BlockCmds.CountBlockInstance();
+        [CommandMethod("ZSELECT")]
+        public static void ZSELECT()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            using(var trans = doc.TransactionManager.StartOpenCloseTransaction())
+            {
+                var setRs = doc.Editor.GetSelection();
+                if (setRs.Status != PromptStatus.OK) return;
+                var circles = from id in setRs.Value.GetObjectIds()
+                              let obj = trans.GetObject(id, OpenMode.ForRead) as Circle
+                              where obj != null
+                              select obj;
+
+                var items = new List<ObjectId>();
+
+                foreach (var c in circles)
+                {
+                    var txt = from id in setRs.Value.GetObjectIds()
+                              let ent = trans.GetObject(id, OpenMode.ForRead) as DBText
+                              where ent != null
+                              where (c.Bounds.Value.MinPoint.X < ent.Position.X
+                              && ent.Position.X < c.Bounds.Value.MaxPoint.X
+                              && c.Bounds.Value.MinPoint.Y < ent.Position.Y
+                              && ent.Position.Y < c.Bounds.Value.MaxPoint.Y)
+                              || ent.TextString.ToLower() == "a"
+                              || ent.TextString.ToLower() == "b"
+                              || ent.TextString.ToLower() == "c"
+
+                              select id;
+                    items.AddRange(txt);
+                } 
+                doc.Editor.SetImpliedSelection(items.ToArray());
+
+            }
+        }
     }
 }
---
+++ Capitalino/Capitalino.Traiano/Block/BlockCmds.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+namespace Capitalino.Traiano.Block
+{
+    internal static class BlockCmds
+    {
+        private static Document doc => Application.DocumentManager.MdiActiveDocument;
+        internal static void CountBlockInstance()
+        {
+            try
+            {
+                using (var trans = doc.TransactionManager.StartTransaction())
+                {
+                    var sampleRs = doc.Editor.GetEntity("Pick up a BlockReferencEntity:");
+                    if (sampleRs.Status != PromptStatus.OK)
+                        return;
+
+                    var sampleId = sampleRs.ObjectId;
+                    var sample = trans.GetObject(sampleId, OpenMode.ForRead) as BlockReference;
+                    if (sample is null || sample.BlockTableRecord.IsNull)
+                    {
+                        doc.Editor.WriteMessage("\nNot a valid BlockReference!");
+                        return;
+                    }
+
+                    var setRs = doc.Editor.GetSelection();
+                    if (setRs.Status != PromptStatus.OK) 
+                        return;
+
+                    var brs = from id in setRs.Value.GetObjectIds()
+                              let br = trans.GetObject(id, OpenMode.ForRead) as BlockReference
+                              where br != null && !br.BlockTableRecord.IsNull && br.BlockTableRecord == sample.BlockTableRecord
+                              select id;
+
+                    var n = brs.Count();
+                    doc.Editor.WriteMessage($"\nThe Count of the identical BlockReference is: {n}");
+                    Clipboard.SetText(n.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
